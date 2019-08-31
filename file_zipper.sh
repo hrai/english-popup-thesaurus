aa5ee710bd9b90f867c2e6297425d6f6806d2ee7
Hangjit Rai <rai.hangjit@gmail.com>
2019-08-31 14:57:16 +1000

    Added File_zipper.sh

---
+++ file_zipper.sh
@@ -0,0 +1,10 @@
+PUBLISHED_FILE=publish.zip
+
+if [ -f "$PUBLISHED_FILE" ]; then
+    echo "Deleting $PUBLISHED_FILE..."
+    rm -f $PUBLISHED_FILE
+fi
+
+zip -r9 $PUBLISHED_FILE background.js content.js dependencies icons LICENSE.txt manifest.json META-INF options popup
+
+echo "Successfully zipped to '$PUBLISHED_FILE'"
